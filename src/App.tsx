8c661d25d525577c35b5b5907fbfa5da3775df0c
MOHAMMED FAROOK R <rhmohammedfarook@gmail.com>
2024-12-24 19:22:41 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import News from "./Components/News"
+function App() {
+  
+
+  return (
+    <div>
+      <News></News>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/News.tsx
@@ -0,0 +1,202 @@
+import React from "react";
+import "./News.css";
+import img1 from "../assets/images/Indian.webp";
+import img2 from "../assets/images/rohit.webp";
+import img3 from "../assets/images/vv.jpeg";
+
+const News = () =>{
+    return(
+        <div className="Mainnews">
+            <header className="hd1">
+                <div className="hc1">Quis nostrud exercitatio nullamco laboris</div>
+                <div className="hc2">
+                    <h1>NewsPaper</h1>
+                </div>
+                <div className="hc3">
+                    <h3 >Lorem Ipsum</h3>
+                    <p>dolor sit amet consectetur adipisicing elit, 
+                        sed do eiusmod tempor incididunt ut labore</p>
+                </div>
+                
+            </header>
+            <hr className="hr1"></hr>
+            <div className="date">FRIDAY, NOVEMBER 24, 2024</div>
+            <hr></hr>
+            
+            <h1 className="head1">Lorem ipsum dolor</h1>
+
+            <div className="secondarycon">
+                <p>Lorem ipsum, dolor sit amet porro culpa laborum aliquam unde ullam eligendi voluptatibus aliquid minus nulla fuga, recusandae deserunt!</p>    
+            </div>
+            
+            {/* --------------------------Grid Content----------------------- */}
+
+
+            <section className="maingird">
+                <div className="mgird1">
+                    <img src={img1} className="mgimg"></img>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, in? Aliquid fuga corporis praesentium illo, delectus enim. Ullam pariatur tempore magnam voluptas? 
+                        Qui numquam explicabo vero modi! Saepe, odio totam.</p>
+                
+
+                    <div className="grid111">
+
+                        <div>
+                            <h1>Ut enim ad<br></br> minim veniam</h1>
+                            <h2>Eiusmod tempor incididunt ut labore</h2>
+                            <div>
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tenetur commodi rerum labore asperiores repellat atque, sit ex, nihil, harum voluptas praesentium 
+                                dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officia 
+                                quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam autem nulla natus minus dolorum quisquam est 
+                                quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam autem nulla natus minus dolorum quisquam est 
+                                et sit quam sed iusto ratione animi quaerat optio eveniet error voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tenetur commodi rerum labore asperiores repellat atque, sit ex, nihil, harum voluptas praesentium 
+                                dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officia 
+                                quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam autem nulla natus minus dolorum quisquam est 
+                                quasLorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tenetur commodi rerum labore asperiores repellat atque, sit ex, nihil, harum voluptas praesentium 
+                                dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officia 
+                                quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam autem nulla natus minus dolorum quisquam est 
+                                quas hic nemo doloribus debitishic nemo doloribus debitishic nemo doloribus debitishic nemo doloribus debitishic nemo doloribus debitishic nemo doloribus debitishic nemo doloribus debitishic nemo doloribus debitis
+                            </div>
+                        </div>
+                                               
+                        <div className="grid111d">
+
+                            <div className="grid111dd">
+                                <h1>Duis aute irure dolor in reprehenderit</h1>
+                            </div>
+
+                            <div className="gridgrid">
+                                <div>
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tenetur commodi rerum labore asperiores repellat atque, sit ex, nihil, harum voluptas praesentium 
+                                    dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officia 
+                                    quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam autem nulla natus minus dolorum quisquam est  
+                                </div>
+                                <div>
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tenetur commodi rerum labore asperiores repellat atque, sit ex, nihil, harum voluptas praesentium 
+                                    dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officia 
+                                    quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam autem nulla natus minus dolorum quisquam est  
+                                </div>
+                                <div>
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tenetur commodi rerum labore asperiores repellat atque, sit ex, nihil, harum voluptas praesentium 
+                                    dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officia 
+                                    quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam autem nulla natus minus dolorum quisquam est  
+                                </div>
+                            </div>
+
+
+                            <div className="grid111dd1">
+                                <h1 className="headhead">Duis aute irure</h1>
+                            </div>
+
+                            <div className="gridgrid1">
+                                <div>
+                                    <h1>Occaecat cupidatat</h1>
+                                    dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officia 
+                                    quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam autem nulla natus minus dolorum quisquam est  
+                                </div>
+                                <div>
+                                    <h1>Exercitation ullamco</h1>
+                                    dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officia 
+                                    quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam autem nulla natus minus dolorum quisquam est  
+                                </div>
+                                <div className="gridgrid1imgdiv">
+                                    <img src={img2} className="gridgrid1img"></img>
+                                </div>
+                            </div>
+
+                            
+
+                            <div className="gridgrid1">
+                                <div>
+                                <h1>Duis aute</h1>
+
+                                    dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officia 
+                                    quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam   
+                                </div>
+                                <div>
+                                <h1>Incididunt ut</h1>
+
+                                    dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officia 
+                                    quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam    
+                                </div>
+                                <div>
+                                <h1>Cillum dolore</h1>
+
+                                    dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officia 
+                                    quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam    
+                                </div>
+                            </div>
+
+                            
+
+                        </div>
+
+                    </div>    
+                
+                </div>
+                
+                <div className="mgird2">
+                    
+                    <h1>
+                    Excepteur sint occaecat
+                    cupidatat non
+                    </h1>
+                    <div className="mgg1">
+                        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tenetur commodi rerum labore asperiores repellat atque, sit ex, nihil, harum voluptas praesentium 
+                            dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officia 
+                            quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam autem nulla natus minus dolorum quisquam est 
+                            quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam autem nulla natus minus dolorum quisquam est 
+                            et sit quam sed iusto ratione animi quaerat optio eveniet error voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        </div>
+                        <div>
+                            <div>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, nihil aliquid totam, accusantium culpa eius delectus iste adipisci officia iure unde atque! Eveniet, quisquam doloribus. Atque voluptate magni labore aut. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde autem non cum explicabo quod, voluptatum quam. Iusto minima obcaecati neque illo hic vita  
+                            </div>
+                            <img src={img3} className="mg2img"></img>
+                            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                        </div>
+                    
+                    
+                    </div>
+
+                    <div className="mgg2">
+                        <h1>Sunt in culpa qui
+                        officia deserunt</h1>
+                    </div>
+                    <div className="grid22">
+                        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tenetur commodi rerum labore asperiores repellat atque, sit ex, nihil, harum voluptas praesentium 
+                            dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officia 
+                            quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam autem nulla natus minus dolorum quisquam est 
+                            quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam autem nulla natus minus dolorum quisquam est 
+                        </div>
+                        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tenetur commodi rerum labore asperiores repellat atque, sit ex, nihil, harum voluptas praesentium 
+                            dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officia 
+                            quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam autem nulla natus minus dolorum quisquam est 
+                            quas fuga iste, culpa dicta consequatur earum hic nemo doloribus debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, molestias aperiam autem nulla natus minus dolorum quisquam est 
+                        </div>
+                    </div>
+
+                    <div className="mgg2">
+                        <h1>Excepteur</h1>
+                    </div>
+                    <div className="grid22">
+                        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tenetur commodi rerum labore asperiores repellat atque, sit ex, nihil, harum voluptas praesentium 
+                            dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officiaMaiores nobis minima libero quis, explicabo officiaMaiores nobis minima libero quis, explicabo officia 
+                        </div>
+
+                        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tenetur commodi rerum labore asperiores repellat atque, sit ex, nihil, harum voluptas praesentium 
+                            dolores error! Id accusantium corrupti quam sequi iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptas a recusandae? Maiores nobis minima libero quis, explicabo officiaMaiores nobis minima libero quis, explicabo officiaMaiores nobis minima libero quis, explicabo officia 
+                        </div>
+                        
+                    </div>
+
+                </div>
+
+            </section>
+
+        </div>
+    )
+}
+
+export default News;
